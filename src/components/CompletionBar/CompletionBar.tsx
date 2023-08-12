5b1d18b5a2bb8eabc9d6341452c4df3ef3e8a5a2
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2023-08-12 16:16:29 -0400

    check for accessibility and screen reader issues

--- src/components/CompletionBar/CompletionBar.tsx
+++ src/components/CompletionBar/CompletionBar.tsx
@@ -13,7 +13,7 @@ type CompletionBarProps = {
  * A bar which shows how many questions have been answered and how many are left. It can take up to 3 children which will be displayed underneath it to its left, middle and right. Note that they will be positioned in the order that they are added
  * @param progress The current value of the meter element, update this to change the meter
  * @param minMax The min value and max value of the meter. Note that min is the first element of the array and max is the second
- * @param label The text which will go into the label element (Note that the label has a class of sr-only which hides it from sighted users but not screen readers)
+ * @param label Text which describes what is being measured by the meter. Note that it will only be visible to screen readers, not to use sighted users
  * @param styles The styles in which to pass on to the meter, should be a string of space separated class names
  * @example
  * <CompletionBar progress={incrementProgress} minMax=[0, 100] label="Remaining Questions" styles="bar bar--green">
@@ -29,18 +29,15 @@ function CompletionBar({
 }: CompletionBarProps) {
   return (
     <Wrapper>
-      <form>
-        <label className='sr-only' htmlFor='progress-bar'>
-          {label}
-        </label>
+      <div>
+        <span className='sr-only'>{label}</span>
         <meter
           className={styles}
-          id='progress-bar'
           min={minMax[0]}
           max={minMax[1]}
           value={progress}
         ></meter>
-      </form>
+      </div>
 
       <ChildLayout>{children}</ChildLayout>
     </Wrapper>
